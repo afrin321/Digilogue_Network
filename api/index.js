d229a8964536bb87e5a0212455b4c639abc17af9
AfrinH <afrinhaque@outlook.com>
2023-08-24 08:05:14 +0600

    update

--- api/index.js
+++ api/index.js
@@ -15,7 +15,9 @@ const uri = process.env.MONGODB_URI;
 const start = async () => {
   try {
     await mongoose.connect(uri);
-    app.listen(4000, () => console.log("Server started on port 4000"));
+    app.listen(4000 || process.env.port, () =>
+      console.log("Server started on port 4000")
+    );
   } catch (error) {
     console.error(error);
     process.exit(1);
